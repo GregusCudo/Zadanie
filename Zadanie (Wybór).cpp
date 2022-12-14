2cf916aa3983ce73b3cf234893f14b1c5cc015c3
GregusCudo <120098192+GregusCudo@users.noreply.github.com>
2022-12-14 16:47:35 +0100

    Add files via upload

---
+++ Zadanie (Wybór).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 10;
+    int liczby[n] = {6,1,2,5,7,3,8,9,4,5};
+    int min = 99999;
+    int max = -1;
+    for (int i = 0; i < n - 1; i += 2)
+    {
+        if (liczby[i] > liczby[i+1])
+    {
+    if (liczby[i] > max)
+       {
+        max = liczby[i];
+        }
+        if (liczby[i+1] < min)
+        {
+        min = liczby[i+1];
+    }
+        }
+        else
+        {
+            if (liczby[i+1] > max)
+          {
+       max = liczby[i+1];
+        }
+            if (liczby[i] < min)
+    {
+    min = liczby[i];
+    }
+    }
+    }
+    if (n % 2 == 1)
+    {
+    if (liczby[n-1] > max)
+    {            max = liczby[n-1];
+    }
+       if (liczby[n-1] < min)
+    {            min = liczby[n-1];
+    }
+    }
+    cout << "Liczba min to: " << min << endl;
+    cout << "Liczba max to: " << max << endl;
+    system("PAUSE >NUL");
+    return 0;
+}
